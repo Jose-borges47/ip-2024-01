443f745ed49a561091dbf5a7f8001367175980fd
jose-borges47 <jose_borges@discente.ufg.br>
2024-06-29 12:18:54 -0300

    adicionando testezinho 3

--- testezinho3/teste1.go
+++ testezinho3/teste1.go
@@ -1,17 +1,27 @@
 package main
 
 import (
-	"bufio"
-	"os"
+    "bufio"
+    "fmt"
+    "os"
 )
 
-func main() {
-	var invertido []string
-	for {
-		input, _ := bufio.NewReader(os.Stdin).ReadString()
-		if input == "#" {
-			break
-		}
+func inverter(input string) string {
+    x := []rune(input)
+    for i, a := 0, len(x)-1; i < a; i, a = i+1, a-1 {
+        x[i], x[a] = x[a], x[i]
+    }
+    return string(x)
+}
 
-	}
+func main() {
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Println("Digite a frase ao contrário:")
+
+    input, _ := reader.ReadString('\n')
+
+    input = input[:len(input)-1]
+
+    invertido := inverter(input)
+    fmt.Println("Frase ao contrário:", invertido)
 }
--- testezinho3/teste2.go
+++ testezinho3/teste2.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func troca(vetor []int, i, j int) {
+	vetor[i], vetor[j] = vetor[j], vetor[i]
+}
+
+func trocaOpostosSeMenor(vetor []int, tamanho int) {
+	for i := 0; i < tamanho/2; i++ {
+		oposto := tamanho - 1 - i
+		if vetor[i] < vetor[oposto] {
+			troca(vetor, i, oposto)
+		}
+	}
+}
 
 func main() {
-	var 
+	var numCasos int
+	fmt.Scan(&numCasos)
+
+	for i := 0; i < numCasos; i++ {
+		var n int
+		fmt.Scan(&n)
+
+		vetor := make([]int, n)
+		for j := 0; j < n; j++ {
+			fmt.Scan(&vetor[j])
+		}
+
+		trocaOpostosSeMenor(vetor, n)
+
+		for j := 0; j < n; j++ {
+			if j > 0 {
+				fmt.Print(" ")
+			}
+			fmt.Print(vetor[j])
+		}
+		fmt.Println()
+	}
 }
