eb75251a00f84b1e0a2990c544f3639e4f8f7e42
Jose-borges47 <jose_borges@discente.ufg.br>
2024-06-28 21:33:55 -0300

    n sei

---
+++ testezinho3/teste1.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+func main() {
+	invertido := []string
+	for {
+		input, _ := bufio.NewReader(os.Stdin).ReadString()
+		if input == "#" {
+			break
+		}
+		
+
+	}
+}
