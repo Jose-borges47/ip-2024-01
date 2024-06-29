3923df045d9a7e5db4eba6774a680fe196e8b7e0
jose-borges47 <jose_borges@discente.ufg.br>
2024-06-29 13:34:19 -0300

    movendo testezinho

--- testezinho3/teste2.go
+++ testezinho3/teste2.go
@@ -8,14 +8,8 @@ func troca(vetor []int, i, j int) {
 	vetor[i], vetor[j] = vetor[j], vetor[i]
 }
 
-func trocaOpostosSeMenor(vetor []int, tamanho int) {
-	for i := 0; i < tamanho/2; i++ {
-		oposto := tamanho - 1 - i
-		if vetor[i] < vetor[oposto] {
-			troca(vetor, i, oposto)
-		}
-	}
-}
+func trocaOpostosSeMenor()
+	
 
 func main() {
 	var numCasos int
@@ -25,19 +19,5 @@ func main() {
 		var n int
 		fmt.Scan(&n)
 
-		vetor := make([]int, n)
-		for j := 0; j < n; j++ {
-			fmt.Scan(&vetor[j])
-		}
-
-		trocaOpostosSeMenor(vetor, n)
-
-		for j := 0; j < n; j++ {
-			if j > 0 {
-				fmt.Print(" ")
-			}
-			fmt.Print(vetor[j])
-		}
-		fmt.Println()
 	}
 }
