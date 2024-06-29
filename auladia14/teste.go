2399b39d6fcd54caeef2838c31f0be5a374a7b78
Jose-borges47 <jose_borges@discente.ufg.br>
2024-06-28 21:23:26 -0300

    n sei

---
+++ auladia14/teste.go
@@ -0,0 +1,47 @@
+//package main
+
+//import "fmt"
+
+//func main() {
+//	var x, n int
+//	fmt.Scan(&x, &n)
+//	fmt.Println(recurção(x, n))
+//}
+
+//func recurção(x, n int) int {
+//	if n == 1 {
+//		return x
+//	} else {
+//		return x * recurção(x, n-1)
+//	}
+//}
+
+//package main
+
+//import "fmt"
+
+//func main() {
+//	coisa := []float64{1.0, 2.2, 3.3, 8.6, 6.2}
+//	tudo := soma(coisa, 0)
+//	fmt.Println(tudo)
+//}
+
+//func soma(x []float64, i int) float64 {
+//	if i == len(x) {
+//		return 0
+//	}
+//	return x[i] + soma(x, i+1)
+//}
+
+package main
+
+import "fmt"
+
+func main() {
+	array := []int{1, 2, 3, 4, 5, 6}
+
+}
+
+func trocadelado() {
+
+}
